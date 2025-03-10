22470988b42e096641aec7e47d64792fd42c2559
Gurupadmaja Merimekala <131772187+Gurupadmaja@users.noreply.github.com>
2025-03-10 17:57:25 +0530

    Add files via upload

---
+++ nonpremitive.js
@@ -0,0 +1,11 @@
+arr=[1,2,3,4,5,6,7,8,8,10]
+console.log(arr[4]);
+
+fruits=["apple","banana","mango"];
+fruits.push("grapes")
+console.log(fruits)
+fruits.unshift("Orange")
+console.log(fruits)
+
+nums=[10,20,30,40,50]
+console.log(nums.includes(30));
